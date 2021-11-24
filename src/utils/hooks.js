04e2c8e32a6dfd9053905535e9a4eb4433ee3a3a
Brint E. Kriebel <github@bekit.net>
2021-11-24 11:51:32 -0800

    hooks: Delay moving the captions in case the camera view has moved
    If we trigger immediately, the position may not be updated yet and we
    may attach to the wrong location. Sleep for 100ms to give it time to
    settle.

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -13,8 +13,10 @@ Hooks.once("ready", () => {
   ui.captions.render(true);
 });
 
-Hooks.on("renderCameraViews", (cameraViews, html) => {
+Hooks.on("renderCameraViews", async (cameraViews, html) => {
   if (ui.captions) {
+    // Sleep to make sure the camera view is in it's final position
+    await new Promise((resolve) => setTimeout(resolve, 100));
     ui.captions.moveAboveCameraViews(html);
   }
 });
